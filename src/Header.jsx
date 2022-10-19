ea741caa3dd34df22f869017a86c560ed45b3ffd
Borodin1 <borodin1902@gmail.com>
2022-10-19 18:49:09 +0300

    error StreamEdit

--- src/Header.jsx
+++ src/Header.jsx
@@ -6,11 +6,11 @@ const Header = () => {
   return (
     <div className="ui secondary pointing menu">
       <Link to="/" className="item">
-        Streams
+        Streamy
       </Link>
       <div className="right menu">
         <Link to="/" className="item">
-          Streamers
+          All Streams
         </Link>
         <GoogleAuth />
       </div>
--- src/streams/StreamEdit.jsx
+++ src/streams/StreamEdit.jsx
@@ -1,7 +1,39 @@
 import React from "react";
+import { connect } from "react-redux";
+import { fetchStream, editStream } from "../action/index";
+import StreamForm from "./StreamForm";
 
-const StreamEdit = () => {
-  return <div>StreamEdit</div>;
+class StreamEdit extends React.Component {
+  componentDidMount() {
+    this.props.fetchStream(this.props.match.params.id);
+  }
+
+  onSubmit = (formValues) => {
+    console.log(formValues);
+  };
+
+  render() {
+    console.log(this.props.stream);
+    if (!this.props.stream) {
+      return <div>Loading ...</div>;
+    }
+
+    return (
+      <div>
+        <h3>Edit a Stream</h3>
+        <StreamForm
+          initialValues={this.props.stream}
+          onSubmit={this.onSubmit}
+        />
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state, ownProps) => {
+  return { stream: state.streams[ownProps.match.params.id] };
 };
 
-export default StreamEdit;
+export default connect(mapStateToProps, { fetchStream, editStream })(
+  StreamEdit
+);
